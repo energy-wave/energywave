91a911667552e523177390b0c3e6f1611ed65edd
energywave <515023802@qq.com>
2019-08-06 14:43:35 +0800

    dev开发

---
+++ src/views/VuePress/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <section>
+        <div>11111</div>
+    </section>
+</template>
+<script>
+    export default {
+        name: "p2",
+        data() {
+            return {
+                songId: ""
+            };
+        },
+        created() {
+            this.text();
+        },
+        methods: {
+            text() {
+                console.log(this.songId);
+            }
+        }
+    };
+</script>
+<style lang="less" scoped>
+</style>
